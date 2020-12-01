a35b45cd011d1217da21140d2ffc899c41608e3a
PedruuH <pedro.inventador@hotmail.com>
2020-12-01 18:56:22 -0300

    .

--- Aula 5/aula/src/Display.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default propos => {
-    return(
-        <div>
-            <h3>Valor Atual: {propos.numero}</h3>
-        </div>
-    )
-}
---
+++ Calculadora/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Calculator from './Calculator'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div>
+        <Calculator />
+    </div>
+      </div>
+    );
+  }
+}
+
+export default App;
